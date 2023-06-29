a9563610511abaf66a811299419f0370a379d1c6
Prata <contato.pedroprata@gmail.com>
2023-06-29 17:28:59 -0300

    Um passo atrás, com refatoração e tdd

--- main.py
+++ main.py
@@ -1,72 +1,29 @@
-''' Verificador de diferenças de string '''
-
-''' Verifica em strings de mesmo tamanho'''
-
-''' Verifica strings de tamanhos diferentes com apenas 1 ocorrência'''
+'''Verifica strings de tamanhos iguais e destaca diferenças entre [] com várias ocorrências'''
 
 def ver(fa,fb):
-    novafb, novafa, difsa, difsb = '','', '', ''
-    try:
-        if len(fa) > len(fb):
-            for i in range(len(fa)):
-                
-                if fa[i] != fb[i]:
-                    difsa += fa[i]
-                    difsb += fb[i]
-                    continue
+    fna, fnb = '', ''
+ 
+    for i in range(len(fa)):
+        if fa[i] != fb[i]:
+            fna += f'[{fb[i]}]'
+            fnb += f'[{fa[i]}]'
+            continue
+        
+        fna += fa[i]
+        fnb += fa[i]
 
-                if difsa and difsb:
-                    novafa += f'[{difsa}]' + fa[i] 
-                    novafb += f'[{difsb}]' + fb[i]
-                    difsa, difsb = '', ''
-                else:
-                    novafa += f'{difsa}' + fa[i] 
-                    novafb += f'{difsb}' + fb[i]
+    return fna + fnb
 
-        else:
-            for i in range(len(fb)):
-                
-                if fb[i] != fa[i]:
-                    difsa += fa[i]
-                    difsb += fb[i]
-                    continue
-                
-                if difsa and difsb:
-                    novafa += f'[{difsa}]' + fa[i] 
-                    novafb += f'[{difsb}]' + fb[i]
-                    difsa, difsb = '', ''
-                else:
-                    novafa += f'{difsa}' + fa[i] 
-                    novafb += f'{difsb}' + fb[i]
-                
-    except IndexError:
-        dif = 0
-        if len(fa) > len(fb):
-            dif = len(fa) - len(fb)
-            novafb += f'[{fa[-dif:]}]'
-            novafa += '[]'
-        else:
-            dif = len(fb) - len(fa)
-            novafa += f'[{fb[-dif:]}]'
-            novafb += '[]'
+assert ver('abcd','accb') == 'a[c]c[b]a[b]c[d]'
 
-    if difsa:
-        return novafa + f'[{difsb}]' + novafb + f'[{difsa}]'
-    else:
-        return novafa + novafb
+assert all((ver('kj','lj') == '[l]j[k]j',
+            ver('oo','io') == '[i]o[o]o',
+            ver('ab','bb') == '[b]b[a]b'))
 
-assert all((ver('abcd','abef') == 'ab[ef]''ab[cd]',
-            ver('olae','olea') == 'ol[ea]''ol[ae]',
-            ver('xyzw','xywz') == 'xy[wz]''xy[zw]'))
+assert all((ver('oi','oy') == 'o[y]o[i]',
+            ver('ab','ac') == 'a[c]a[b]',
+            ver('aa','ab') == 'a[b]a[a]',))
 
-assert all((ver('abcd','abc') == 'abc[]''abc[d]',
-            ver('123','1234') == '123[4]''123[]',
-            ver('olá?','olá') == 'olá[]''olá[?]'))
-
-assert all((ver('ab','ac') == 'a[c]''a[b]',
-            ver('12','13') == '1[3]''1[2]',
-            ver('oi','oy') == 'o[y]''o[i]'))
-
-assert all((ver('a','b') == '[b]''[a]',
-            ver('c','d') == '[d]''[c]',
-            ver('e','f') == '[f]''[e]'))
+assert all((ver('b','c') == '[c][b]',
+            ver('1','2') == '[2][1]',
+            ver('a','b') == '[b][a]'))
